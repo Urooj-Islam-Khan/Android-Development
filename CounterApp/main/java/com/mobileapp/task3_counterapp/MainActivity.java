50584575d474b8d0a927a2d40a2a44730d195f7b
UROOJ ISLAM KHAN <128351878+Urooj-Islam-Khan@users.noreply.github.com>
2024-11-27 14:40:19 +0500

    Add files via upload

---
+++ CounterApp/main/java/com/mobileapp/task3_counterapp/MainActivity.java
@@ -0,0 +1,62 @@
+package com.mobileapp.task3_counterapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView number;
+    Button inc, dec,reset;
+    int count=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        number = findViewById(R.id.number);
+        inc = findViewById(R.id.inc);
+        dec = findViewById(R.id.dec);
+        reset = findViewById(R.id.reset);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        inc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count++;
+                number.setText(String.valueOf(count));
+
+            }
+        });
+
+   dec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count--;
+                number.setText(String.valueOf(count));
+
+            }
+        });
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = 0;
+                number.setText(String.valueOf(count));
+            }
+        });
+
+    }
+}
